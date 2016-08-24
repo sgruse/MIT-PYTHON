527ccf774a867a47ca53c2d58e2c4f0919753035
Sam Gruse <sgruse89@gmail.com>
2016-08-24 12:40:07 -0700

    Adding HEaps kalgorightm

--- perm.js
+++ perm.js
@@ -6,6 +6,7 @@ function doPerm(str, arr) {
     if (str.length == 0) permutations.push(arr.join(''));
     for (var i = 0; i < str.length; i++) {
         console.log('STRING ARRAY IS : ', str);
+        console.log('I IS : ', i);
         var x = str.splice(i, 1);
         console.log('SPLICED IS : ', x);
         arr.push(x);
@@ -13,9 +14,44 @@ function doPerm(str, arr) {
         console.log('POP HIT ON : ', arr, ' AND STRING ARRAY IS : ', str);
         arr.pop();
         str.splice(i, 0, x);
-        console.log('I IS : ', i);
         console.log('STRING ARRA IS : ', str)
     }
 }
 doPerm('ab', []);
 console.log(permutations);
+
+
+// BETTER WAY AND UTILIZING HEAPS ALGORITHM
+
+function perms(data) {
+    if (!(data instanceof Array)) {
+        throw new TypeError("input data must be an Array");
+    }
+
+    data = data.slice();  // make a copy
+    var permutations = [],
+        stack = [];
+
+    function doPerm() {
+        if (data.length == 0) {
+            permutations.push(stack.slice());
+        }
+        for (var i = 0; i < data.length; i++) {
+            var x = data.splice(i, 1);
+            stack.push(x);
+            doPerm();
+            stack.pop();
+            data.splice(i, 0, x);
+        }
+    }
+
+    doPerm();
+    return permutations;
+}
+
+var input = "abcd".split('');
+var result = perms(input);
+for (var i = 0; i < result.length; i++) {
+    result[i] = result[i].join('');
+}
+console.log(result);
