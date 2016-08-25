3b8a3dd74e23696d6fcf7d87a2eee8db757fc6fa
Sam Gruse <sgruse89@gmail.com>
2016-08-25 12:10:43 -0700

    Rotating Matrix

--- perm.js
+++ perm.js
@@ -119,3 +119,53 @@ function check (arr1, arr2) {
 }
   return true;
 }
+
+// Rotating matrix
+
+var grid = [
+  [0,0], [0,1], [0,2], [0,3],
+  [1,0], [1,1], [1,2], [1,3],
+  [2,0], [2,1], [2,2], [2,3],
+  [3,0], [3,1], [3,2], [3,3]
+];
+
+var newGrid = [];
+var rowLength = Math.sqrt(grid.length);
+newGrid.length = grid.length
+
+for (var i = 0; i < grid.length; i++)
+{
+    //convert to x/y
+    var x = i % rowLength;
+    var y = Math.floor(i / rowLength);
+
+    //find new x/y
+    var newX = rowLength - y - 1;
+    var newY = x;
+
+    //convert back to index
+    var newPosition = newY * rowLength + newX;
+    newGrid[newPosition] = grid[i];
+}
+
+for (var i = 0; i < newGrid.length; i++)
+{
+    console.log(newGrid[i])
+}
+
+//  Simplified
+
+var grid = [
+  [0,0], [0,1], [0,2], [0,3], [0,4],
+  [1,0], [1,1], [1,2], [1,3], [1,4],
+  [2,0], [2,1], [2,2], [2,3], [2,4],
+  [3,0], [3,1], [3,2], [3,3], [3,4],
+  [4,0], [4,1], [4,2], [4,3], [4,4]
+];
+
+var side = Math.sqrt(grid.length);
+
+var rotate = function(d,i){
+   return [Math.abs(i % side - side+1), Math.floor(i/side)]
+}
+grid = grid.map(rotate);
