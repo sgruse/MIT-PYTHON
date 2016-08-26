fbd9c7649fa319981c3c3b1bb3d918c49b138685
Sam Gruse <sgruse89@gmail.com>
2016-08-26 15:00:33 -0700

    Added the code for remove node

--- linkedList.js
+++ linkedList.js
@@ -32,10 +32,73 @@ SinglyList.prototype.add = function (value) {
   return node;
 }
 
+// TO SEARCH NODE A CERTAIN POSITION
+
+SinglyList.prototype.searchNodeAt = function(position) {
+  var currentNode = this.head;
+  var length = this._length;
+  var count = 1;
+  var message = {failure: 'Failure: This node is non-existant in the list'};
+
+  // First case is invalid position
+  if (length === 0 || position < 1 || position > length) {
+    throw new Error(message.failure);
+  }
+
+  // Valid position
+  while (count < position) {
+    currentNode = currentNode.next;
+    count ++;
+  }
+  return currentNode;
+}
+
+SinglyList.prototype.remove = function(position) {
+  var currentNode = this.head;
+  var count = 1;
+  var length = this._length;
+  var message = {failure: 'Failure: This node is non-existant in the list'};
+  var beforeNodeToDelete = null;
+  var nodeToDelete = null;
+  var deleteNode = null;
+
+// INVALIDE POSITION
+  if (position < length || position > length) {
+    throw new Error(message.failure);
+  }
+
+// IF THE FIRST NODE IS DELETED
+  if (position === 1) {
+    this.head = currentNode.next;
+    deleteNode = currentNode;
+    currentNode = null;
+    this._length --;
+    return deleteNode;
+  }
+
+// ANY OTHER NODE IS REMOVED
+  while (count < position) {
+    beforeNodeToDelete = currentNode
+    nodeToDelete = currentNode.next;
+    count ++;
+  }
+
+  beforeNodeToDelete.next = nodeToDelete.next;
+  deleteNode = nodeToDelete;
+  nodeToDelete = null;
+  this._length --;
+
+  return deleteNode;
+}
+
 
 var x = new SinglyList();
 
 x.add('sam');
 x.add('bobby');
+
+console.log("NODE AT POSITION IS : ", x.searchNodeAt(2));
+
+console.log('DELETE NODE AT POSITION : ', x.remove(2));
 
 console.log(x);
