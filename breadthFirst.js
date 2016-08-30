07d9ca50d60b56c41f8503e60c11521b0b359748
Sam Gruse <sgruse89@gmail.com>
2016-08-30 11:42:04 -0700

    changing the breadthFrist

--- breadthFirst.js
+++ breadthFirst.js
@@ -1,3 +1,64 @@
+//
+// var Queue = function() {
+//   this.items = [];
+// };
+//
+// Queue.prototype.enQueue = function(val) {
+//   this.items.push(val);
+// }
+//
+// Queue.prototype.deQueue = function() {
+//   this.items.shift();
+// }
+//
+// Queue.prototype.isEmpty = function() {
+//   return this.items.length === 0;
+// }
+//
+// var doBfs = function(graph, source) {
+//   var bfsInfo = [];
+//
+//   for (var i = 0; i < graph.length; i++) {
+//     bfsInfo[i] = {
+//       distance: null,
+//       predecessor: null }
+//   }
+//   bfsInfo[source].distance = 0;
+//
+//   var queue = new Queue();
+//   queue.enQueue(source);
+//
+//   var current;
+//   var v;
+//   var adjList;
+//
+//   while (!queue.isEmpty()) {
+//     current = queue.deQueue();
+//     adjList = graph[current];
+//
+//     for (var j = 0; j < ajdList.length; j++) {
+//       v = adjList[j];
+//       if (ajdList[j + 1] !== null) {
+//         queue.enQueue(ajdList[j + 1]);
+//       }
+//       if (adjList[j - 1] !== null) {
+//         queue.enQueue(ajdList[j - 1]);
+//       }
+//
+//     }
+//
+//     v = adjList;
+//
+//   }
+//
+//
+//   return bfsInfo;
+// }
+
+var tree = [2, 4, 1, 5, 7, 8, 9];
+
+
+
 
 var Queue = function() {
   this.items = [];
@@ -15,42 +76,22 @@ Queue.prototype.isEmpty = function() {
   return this.items.length === 0;
 }
 
-var doBfs = function(graph, source) {
-  var bfsInfo = [];
+function bfs(root) {
+  if (root == null) return false;
 
-  for (var i = 0; i < graph.length; i++) {
-    bfsInfo[i] = {
-      distance: null,
-      predecessor: null }
-  }
-  bfsInfo[source].distance = 0;
+  var q = new Queue();
+  q.push(root);
 
-  var queue = new Queue();
-  queue.enQueue(source);
+  while (!q.isEmpty()) {
+    current = q.deQueue;
+    console.log(current.data);
 
-  var current;
-  var v;
-  var adjList;
-
-  while (!queue.isEmpty()) {
-    current = queue.deQueue();
-    adjList = graph[current];
-
-    for (var j = 0; j < ajdList.length; j++) {
-      v = adjList[j];
-      if (ajdList[j + 1] !== null) {
-        queue.enQueue(ajdList[j + 1]);
-      }
-      if (adjList[j - 1] !== null) {
-        queue.enQueue(ajdList[j - 1]);
-      }
-
+    if(current.left !== null) {
+      q.enQueue(current.left);
     }
 
-    v = adjList;
-
+    if(current.right !== null) {
+      q.enQueue(current.right);
+    }
   }
-
-
-  return bfsInfo;
 }
